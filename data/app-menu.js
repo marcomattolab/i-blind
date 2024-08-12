1e3b8ffe4790a01b0f1f4fc7f4c1ca71f9adc529
“Marco <marcomatto@gmail.com>
2024-08-12 20:00:56 +0200

    add Gemini

--- data/app-menu.js
+++ data/app-menu.js
@@ -4,6 +4,7 @@ export default [
     image:
       "http://192.168.1.100:19000/assets/designer_01.jpeg",
     name: "Detailed Voice-Based Environment",
+    question: "Gemini, with its natural language understanding and text generation capabilities, can provide accurate and detailed descriptions of the surrounding environment. For instance, it can describe the location of furniture, doors, windows, electrical outlets, and any other object present in the house.",
     description: "Gemini, with its natural language understanding and text generation capabilities, can provide accurate and detailed descriptions of the surrounding environment. For instance, it can describe the location of furniture, doors, windows, electrical outlets, and any other object present in the house."
   },
   {
@@ -11,6 +12,7 @@ export default [
     image:
       "http://192.168.1.100:19000/assets/designer_02.jpeg",
     name: "Step-by-Step Voice Navigation",
+    question: "The user can specify a destination within the house (e.g., 'Front door', 'Kitchen', 'Bathroom') and Gemini will provide clear and precise voice instructions on how to reach it, avoiding obstacles and indicating the correct direction.",
     description: "The user can specify a destination within the house (e.g., 'Front door', 'Kitchen', 'Bathroom') and Gemini will provide clear and precise voice instructions on how to reach it, avoiding obstacles and indicating the correct direction."
     
   },
@@ -18,12 +20,14 @@ export default [
     id: "2",
     image: "http://192.168.1.100:19000/assets/designer_04.jpeg",
     name: "Voice Command Recognition",
+    question:"The user can interact with the app through simple and intuitive voice commands, such as 'Where is the phone?', 'Turn on the light', 'What is the temperature?",
     description: "The user can interact with the app through simple and intuitive voice commands, such as 'Where is the phone?', 'Turn on the light', 'What is the temperature?'"
   },
   {
     id: "3",
     image: "http://192.168.1.100:19000/assets/designer_07.jpeg",
     name: "Environment Mapping",
+    question:"The user can create a virtual map of their home, indicating the location of objects and points of interest. This map will then be used by Gemini to provide more precise directions.",
     description: "The user can create a virtual map of their home, indicating the location of objects and points of interest. This map will then be used by Gemini to provide more precise directions."
   }
 ];
--- screens/BlindScreen.js
+++ screens/BlindScreen.js
@@ -1,15 +1,50 @@
 import { useNavigation, useRoute } from '@react-navigation/native'
-import { Image, ScrollView, TouchableOpacity, View, Text } from 'react-native'
+import { Image, ScrollView, TouchableOpacity, View, Text, Button, ActivityIndicator } from 'react-native'
 import { Ionicons } from '@expo/vector-icons';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { AntDesign } from '@expo/vector-icons';
 import { FitnessItems } from '../Context';
+import { GoogleGenerativeAI } from '@google/generative-ai';
+
+
 
 const BlindScreen = () => {
   const route = useRoute();
   const navigation = useNavigation();
   const { completed, setCompleted } = useContext(FitnessItems);
+
+
+  // GEMINI
+  const [inputValue, setInputValue] = useState('');
+  const [promptResponses, setpromptResponses] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const genAI = new GoogleGenerativeAI("AIzaSyC4WOa1DL4iWr1zPUy1fYJvT8Ab2f_APJ4");
+
+  const getResponseForGivenPrompt = async () => {
+    try {
+      console.log("# getResponseForGivenPrompt....");
+      setLoading(true)
+      const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+      const questionPrompt= "As "+route.params.name + " can you "+oute.params.question;
+      onsole.log("# questionPrompt = ", questionPrompt);
+      const result = await model.generateContent(questionPrompt);
+      setInputValue('')
+      const response = result.response;
+      onsole.log("# response = ", response);
+      const text = response.text();
+      console.log(text)
+      setpromptResponses([...promptResponses,text]);
+  
+      setLoading(false)
+    }
+    catch (error) {
+      console.log(error)
+      console.log("# Something Went Wrong");
+      setLoading(false)
+    }
+  };
+
 
   return (
     <>
@@ -31,25 +66,41 @@ const BlindScreen = () => {
         />
 
         {
-            
             <TouchableOpacity style={{ marginVertical: 12, marginHorizontal: 18, flexDirection: "row", alignItems: "center", justifyContent: "space-between" }}>
               <View style={{ flexDirection: "row", alignItems: "center", }}>
-                <Image style={{ width: 90, height: 90, }} source={{ uri: "item.image" }} />
-
+                
                 <View style={{ marginLeft: 10 }}>
-                  <Text style={{ fontSize: 18, fontWeight: "bold" }}>{"item.name"}</Text>
-                  <Text style={{ marginTop: 4, fontSize: 16, color: "gray" }}>{"item.sets"}</Text>
+                  <Text style={{ fontSize: 18, fontWeight: "bold" }}>{"As "+"item.name" + " can you "+"item.question"}</Text>
+              
                 </View>
               </View>          
             </TouchableOpacity>
+        
         }
+
+
+
       </ScrollView>
 
-      <TouchableOpacity onPress={() => {
-        navigation.navigate("Fit", { exercises: route.params.exercises }) 
-        setCompleted([]);
-      }} style={{ backgroundColor: "#198f51", padding: 12, marginHorizontal: 15, marginVertical: 20, borderRadius: 50}}>
-        <Text style={{ textAlign: "center", color: "#fff", fontWeight: "bold", fontSize: 20 }}><MaterialCommunityIcons name="whistle" size={24} color="white" /> START</Text>
+
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        {loading ? (
+          <ActivityIndicator size="large" color="#007bff" />
+        ) : (
+          promptResponses.map((promptResponse, index) => (
+            <View key={index} style={{ marginBottom: 10 }}>
+              <Text style={{ fontWeight: index === promptResponses.length - 1 ? 'bold' : 'normal' }}>
+                {promptResponse}
+              </Text>
+            </View>
+          ))
+        )}
+      </View>
+
+
+      <TouchableOpacity onPress={getResponseForGivenPrompt()} 
+         style={{ backgroundColor: "#198f51", padding: 12, marginHorizontal: 15, marginVertical: 20, borderRadius: 50}}>
+        <Text style={{ textAlign: "center", color: "#fff", fontWeight: "bold", fontSize: 20 }}><MaterialCommunityIcons name="whistle" size={24} color="white" /> Ask Gemini!</Text>
       </TouchableOpacity>
     </>
   )

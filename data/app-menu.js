009c684297c39e468fb76b09255d1db8518f4943
“Marco <marcomatto@gmail.com>
2024-08-12 18:52:51 +0200

    minor fix

--- data/app-menu.js
+++ data/app-menu.js
@@ -4,14 +4,14 @@ export default [
     image:
       "http://192.168.1.100:19000/assets/designer_01.jpeg",
     name: "Detailed Voice-Based Environment",
-    description: "Gemini, with its natural language understanding and text generation capabilities, can provide accurate and detailed descriptions of the surrounding environment. For instance, it can describe the location of furniture, doors, windows, electrical outlets, and any other object present in the house.",
+    description: "Gemini, with its natural language understanding and text generation capabilities, can provide accurate and detailed descriptions of the surrounding environment. For instance, it can describe the location of furniture, doors, windows, electrical outlets, and any other object present in the house."
   },
   {
     id: "1",
     image:
       "http://192.168.1.100:19000/assets/designer_02.jpeg",
     name: "Step-by-Step Voice Navigation",
-    description: "The user can specify a destination within the house (e.g., 'Front door', 'Kitchen', 'Bathroom') and Gemini will provide clear and precise voice instructions on how to reach it, avoiding obstacles and indicating the correct direction.",
+    description: "The user can specify a destination within the house (e.g., 'Front door', 'Kitchen', 'Bathroom') and Gemini will provide clear and precise voice instructions on how to reach it, avoiding obstacles and indicating the correct direction."
     
   },
   {
